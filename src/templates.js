b522d70956014f28c39961e1d1415ae009b0577e
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-04-14 20:20:32 +0300

    corected

--- src/templates.js
+++ src/templates.js
@@ -1,14 +1,1 @@
-export function bookCardTemplates({book_image, title, author}) {
-return ( `
-  <div class='shop-card'>
-  <div class='image-block'>
-    <img class='image-block__image' src=${books.book_image} />
-  </div>
-  <div class='shop-card__meta'>
-    <div class='shop-card__title'>${title}</div>
-    <div class='shop-card__author'>${author}</div>
-  </div
-  </div>
-  </div>
-`);
-}
+export function bookCardTemplates()
